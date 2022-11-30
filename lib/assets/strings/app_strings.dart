fe60e80d5f64e2a59d8651fe3a57de2af3de0ebe
Nyanny <EvgenyNedosekin@gmail.com>
2022-11-30 15:30:29 +0300

    feat(SI-2350): added AppStrings to hold there all app strings

---
+++ lib/assets/strings/app_strings.dart
@@ -0,0 +1,10 @@
+class AppStrings {
+  static const onboardingScreenMapPageTitle = 'Добро пожаловать в Путеводитель';
+  static const onboardingScreenMapPageSubitle = 'Ищи новые локации и сохраняй самые любимые.';
+  static const onboardingScreenRoutePageTitle = 'Построй маршрут и отправляйся в путь';
+  static const onboardingScreenRoutePageSubitle = 'Достигай цели максимально быстро и комфортно.';
+  static const onboardingScreenPlacesPageTitle = 'Добавляй места, которые нашёл сам';
+  static const onboardingScreenPlacesPageSubitle = 'Делись самыми интересными и помоги нам стать лучше!';
+  static const skipButton = 'Пропустить';
+  static const startButton = 'НА СТАРТ';
+}
