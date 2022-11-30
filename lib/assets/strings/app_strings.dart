49fd7f08627edd3a6bd4890a601407b627a25a19
Nyanny <EvgenyNedosekin@gmail.com>
2022-11-30 15:35:59 +0300

    refactor(SI-2350): added comments with description

--- lib/assets/strings/app_strings.dart
+++ lib/assets/strings/app_strings.dart
@@ -1,10 +1,14 @@
 class AppStrings {
+  /// Onboarding page with "map" and map image - "first" onboarding page
   static const onboardingScreenMapPageTitle = 'Добро пожаловать в Путеводитель';
   static const onboardingScreenMapPageSubitle = 'Ищи новые локации и сохраняй самые любимые.';
+  /// Onboarding page with "route" and backpack image - "second" onboarding page
   static const onboardingScreenRoutePageTitle = 'Построй маршрут и отправляйся в путь';
   static const onboardingScreenRoutePageSubitle = 'Достигай цели максимально быстро и комфортно.';
+  /// Onboarding page with "places" and tap image - "third" onboarding page
   static const onboardingScreenPlacesPageTitle = 'Добавляй места, которые нашёл сам';
   static const onboardingScreenPlacesPageSubitle = 'Делись самыми интересными и помоги нам стать лучше!';
+  /// Buttons strings
   static const skipButton = 'Пропустить';
   static const startButton = 'НА СТАРТ';
 }
