ed02d1530d90747d44be878eb7f17253d935cdbc
Nyanny <EvgenyNedosekin@gmail.com>
2022-11-30 17:46:04 +0300

    feat(SI-2350): added onboarding Elementary module

---
+++ lib/features/screens/onboarding/onboarding_screen_export.dart
@@ -0,0 +1,4 @@
+/// Exports for router
+export 'onboarding_screen.dart';
+export 'onboarding_screen_model.dart';
+export 'onboarding_screen_wm.dart';
---
+++ lib/features/screens/onboarding/onboarding_screen_model.dart
@@ -0,0 +1,5 @@
+import 'package:elementary/elementary.dart';
+import 'package:surf_study_project/features/screens/onboarding/onboarding_screen.dart';
+
+/// Model for [OnboardingScreen]
+class OnboardingScreenModel extends ElementaryModel {}
