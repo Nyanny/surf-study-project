d9cbec9a7dbc43dbb910ff5e4341c3e6ae1a1dfd
Nyanny <EvgenyNedosekin@gmail.com>
2022-12-06 21:47:36 +0300

    feat(SI-2355): added necessary api strings

---
+++ lib/api/api_strings.dart
@@ -0,0 +1,15 @@
+/// Strings for API
+abstract class ApiStrings {
+  /// base url of the API
+  static const baseUrl = 'https://test-backend-flutter.surfstudio.ru';
+
+  /// additional url for place
+  static const place = '/place';
+
+  /// additional url for filteredPlaces
+
+  static const filteredPlaces = '/filtered_places';
+
+  /// additional url for uploadFile
+  static const uploadFile = '/upload_file';
+}
