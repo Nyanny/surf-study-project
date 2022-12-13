1f300aa08f7e3d627f42786f431b1593d5e604f9
Nyanny <EvgenyNedosekin@gmail.com>
2022-12-14 01:14:21 +0300

    feat: added some features themes

--- lib/features/app/app.dart
+++ lib/features/app/app.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:surf_study_project/assets/themes/app_themes.dart';
+import 'package:surf_study_project/assets/themes/themes_flavours/add_button_colors.dart';
+import 'package:surf_study_project/assets/themes/themes_flavours/loader_colors.dart';
+import 'package:surf_study_project/assets/themes/themes_flavours/onboarding_colors.dart';
+import 'package:surf_study_project/assets/themes/themes_flavours/place_list_colors.dart';
 import 'package:surf_study_project/config/app_config.dart';
 import 'package:surf_study_project/config/environment/environment.dart';
 import 'package:surf_study_project/features/app/di/app_scope.dart';
@@ -41,12 +45,32 @@ class _AppState extends State<App> {
       },
       child: MaterialApp.router(
         /// Themes
-        theme: AppThemes.lightTheme,
-        darkTheme: AppThemes.darkTheme,
+
+        /// light theme
+        theme: AppThemes.lightTheme.copyWith(
+          extensions: <ThemeExtension<dynamic>>[
+            OnboardingColors.light,
+            PlaceListColors.light,
+            AddButtonColors.light,
+            LoaderColors.light,
+          ],
+        ),
+
+        /// dark theme
+        darkTheme: AppThemes.darkTheme.copyWith(
+          extensions: <ThemeExtension<dynamic>>[
+            OnboardingColors.dark,
+            PlaceListColors.dark,
+            AddButtonColors.dark,
+            LoaderColors.dark,
+          ],
+        ),
+
         /// Localization.
         locale: _localizations.first,
         localizationsDelegates: _localizationsDelegates,
         supportedLocales: _localizations,
+
         /// This is for navigation.
         routeInformationParser: _scope.router.defaultRouteParser(),
         routerDelegate: _scope.router.delegate(),
