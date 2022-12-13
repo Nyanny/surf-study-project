9a11dd862387eabb683836ea43ed7200e6437dc5
Nyanny <EvgenyNedosekin@gmail.com>
2022-12-14 01:08:04 +0300

    feat: added onboarding_page_data.dart with Data class that holds info for the OnboardingPage

---
+++ lib/features/onboarding/domain/onboarding_page_data.dart
@@ -0,0 +1,18 @@
+/// Class [OnboardingPageData] is a data class that holds info for the OnboardingPage
+class OnboardingPageData {
+  /// path to the asset
+  final String picturePath;
+
+  /// title
+  final String title;
+
+  /// subtitle
+  final String subtitle;
+
+  /// constructor
+  OnboardingPageData({
+    required this.picturePath,
+    required this.title,
+    required this.subtitle,
+  });
+}
