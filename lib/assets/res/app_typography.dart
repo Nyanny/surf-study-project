52a2bafe690807c75206d9f99cd0aee36240addc
Nyanny <EvgenyNedosekin@gmail.com>
2022-12-15 19:21:00 +0300

    refactor: edited app_typography.dart to work with flutter_screenutil package

--- lib/assets/res/app_typography.dart
+++ lib/assets/res/app_typography.dart
@@ -1,67 +1,68 @@
 import 'package:flutter/rendering.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 /// Class [AppTypography] that holds all [TextStyle] used in the app
 class AppTypography {
   /// [largeTitle] defines Roboto font with, font size 32, line height 36, font weight 700
-  static const largeTitle = TextStyle(
+  static final largeTitle = TextStyle(
     fontFamily: 'Roboto',
-    fontSize: 32.0,
+    fontSize: 32.0.sp,
     height: 36 / 32,
     fontWeight: FontWeight.w700,
   );
 
   /// [title] defines Roboto font with color font size 24, line height 28.8, font weight 700
-  static const title = TextStyle(
+  static final title = TextStyle(
     fontFamily: 'Roboto',
-    fontSize: 24.0,
+    fontSize: 24.0.sp,
     height: 28.8 / 24,
     fontWeight: FontWeight.w700,
   );
 
   /// [subtitle] defines Roboto font with font size 18, line height 24, font weight 500
-  static const subtitle = TextStyle(
+  static final subtitle = TextStyle(
     fontFamily: 'Roboto',
-    fontSize: 18.0,
+    fontSize: 18.0.sp,
     height: 24 / 18,
     fontWeight: FontWeight.w500,
   );
 
   /// [text] defines Roboto font with font size 16, line height 20, font weight 500
-  static const text = TextStyle(
+  static final text = TextStyle(
     fontFamily: 'Roboto',
-    fontSize: 16.0,
+    fontSize: 16.0.sp,
     height: 20 / 16,
     fontWeight: FontWeight.w500,
   );
 
   /// [smallBoldText] defines Roboto font with font size 14, line height 18, font weight 700
-  static const smallBoldText = TextStyle(
+  static final smallBoldText = TextStyle(
     fontFamily: 'Roboto',
-    fontSize: 14.0,
+    fontSize: 14.0.sp,
     height: 18 / 14,
     fontWeight: FontWeight.w700,
   );
 
   /// [smallText] defines Roboto font with font size 14, line height 18, font weight 500
-  static const smallText = TextStyle(
+  static final smallText = TextStyle(
     fontFamily: 'Roboto',
-    fontSize: 14.0,
+    fontSize: 14.0.sp,
     height: 18 / 14,
     fontWeight: FontWeight.w400,
   );
 
   /// [superSmallText] defines Roboto font with font size 12, line height 16, font weight 400
-  static const superSmallText = TextStyle(
+  static final superSmallText = TextStyle(
     fontFamily: 'Roboto',
-    fontSize: 12.0,
+    fontSize: 12.0.sp,
     height: 16 / 12,
     fontWeight: FontWeight.w400,
   );
 
   /// [buttonText] defines Roboto font with font size 14, line height 18, font weight 700, letterSpacing 0.48 (0.03em)
-  static const buttonText = TextStyle(
+  static final buttonText = TextStyle(
     fontFamily: 'Roboto',
-    fontSize: 14.0,
+    fontSize: 14.0.sp,
     height: 18 / 14,
     fontWeight: FontWeight.w700,
     letterSpacing: 0.48,
