e085dbd61021a4110667878202c5ff2f7b23a69c
Nyanny <EvgenyNedosekin@gmail.com>
2023-01-11 21:51:32 +0300

    feat(2360): added MainScreen elementary module

---
+++ lib/features/main_screen/screens/main_tabs_screen/main_screen.dart
@@ -0,0 +1,37 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:elementary/elementary.dart';
+import 'package:flutter/material.dart';
+import 'package:surf_study_project/features/main_screen/screens/main_tabs_screen/main_screen_export.dart';
+
+/// Class [MainScreen] is class for the 'main' feature
+/// It's just a [Scaffold] with [BottomNavigationBar] that uses [AutoTabsRouter.tabBar]
+/// MainScreen feature is handling routes
+class MainScreen extends ElementaryWidget<IMainScreenWidgetModel> {
+  /// constructor
+  const MainScreen({
+    Key? key,
+    WidgetModelFactory wmFactory = mainScreenWmFactory,
+  }) : super(wmFactory, key: key);
+
+  @override
+  Widget build(IMainScreenWidgetModel wm) {
+    return AutoTabsRouter.tabBar(
+      routes: wm.routes,
+      builder: (context, child, controller) {
+        final tabsRouter = context.tabsRouter;
+
+        return Scaffold(
+          body: child,
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: tabsRouter.activeIndex,
+            onTap: tabsRouter.setActiveIndex,
+            items: wm.navigationBarItems,
+          ),
+        );
+      },
+    );
+  }
+}
