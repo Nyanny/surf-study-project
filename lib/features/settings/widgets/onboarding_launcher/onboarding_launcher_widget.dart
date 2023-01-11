87e92dd57903d7dff02de716d71a4530b2d272dd
Nyanny <EvgenyNedosekin@gmail.com>
2023-01-12 01:55:08 +0300

    refactor: changed layout to relative - added flutter_screenutil.dart usage

--- lib/features/settings/widgets/onboarding_launcher/onboarding_launcher_widget.dart
+++ lib/features/settings/widgets/onboarding_launcher/onboarding_launcher_widget.dart
@@ -1,6 +1,7 @@
 import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:surf_study_project/assets/res/app_assets.dart';
 import 'package:surf_study_project/assets/strings/app_strings.dart';
 import 'package:surf_study_project/features/settings/widgets/onboarding_launcher/onboarding_launcher_wm.dart';
@@ -23,13 +24,17 @@ class OnboardingLauncherWidget
   @override
   Widget build(IOnboardingLauncherWidgetModel wm) {
     return ListTile(
+      minVerticalPadding: 12.w,
       title: const Text(
         AppStrings.settingsTutorial,
       ),
       trailing: IconButton(
+        padding: EdgeInsets.zero,
         onPressed: wm.launchOnboarding,
         icon: SvgPicture.asset(
           AppAssets.info,
+          height: 24.w,
+          width: 24.w,
           color: buttonColor,
         ),
       ),
