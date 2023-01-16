187d22c514d2f7c03efec985cbf2434773fdb56d
Nyanny <EvgenyNedosekin@gmail.com>
2023-01-17 02:16:20 +0300

    feat(SI-2366): added place_type_filter.dart

---
+++ lib/features/filter/domain/entity/place_type_filter.dart
@@ -0,0 +1,14 @@
+/// Class [PlaceTypeFilter] holds assets path and name needed for PlaceType in Filter feature
+class PlaceTypeFilter {
+  /// asset path
+  String iconPath;
+
+  /// title
+  String title;
+
+  /// Constructor
+  PlaceTypeFilter({
+    required this.iconPath,
+    required this.title,
+  });
+}
