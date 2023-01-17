1631bf8a89320b8f2474c83291d20f3430f65fad
Nyanny <EvgenyNedosekin@gmail.com>
2023-01-17 15:18:53 +0300

    refactor (SI-2366): refactored enum to enum 2.17

--- lib/features/common/domain/entity/place_type.dart
+++ lib/features/common/domain/entity/place_type.dart
@@ -1,11 +1,37 @@
 // ignore_for_file: public_member_api_docs
 
+import 'package:surf_study_project/assets/res/app_assets.dart';
+import 'package:surf_study_project/assets/strings/app_strings.dart';
+
 /// supported by API types
 enum PlaceType {
-  hotel,
-  restaurant,
-  other,
-  park,
-  museum,
-  cafe,
+  hotel(
+    iconPath: AppAssets.hotel,
+    title: AppStrings.hotelFilterCategory,
+  ),
+  restaurant(
+    iconPath: AppAssets.restaurant,
+    title: AppStrings.restaurantFilterCategory,
+  ),
+  other(
+    iconPath: AppAssets.particularPlace,
+    title: AppStrings.specialPlaceFilterCategory,
+  ),
+  park(
+    iconPath: AppAssets.park,
+    title: AppStrings.parkFilterCategory,
+  ),
+  museum(
+    iconPath: AppAssets.museum,
+    title: AppStrings.museumFilterCategory,
+  ),
+  cafe(
+    iconPath: AppAssets.cafe,
+    title: AppStrings.cafeFilterCategory,
+  );
+
+  const PlaceType({required this.iconPath, required this.title});
+
+  final String iconPath;
+  final String title;
 }
