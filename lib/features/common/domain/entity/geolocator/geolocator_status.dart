3d8a104adf45e34aa581fe9ebf127c023efe5ec6
Nyanny <EvgenyNedosekin@gmail.com>
2023-01-18 16:05:56 +0300

    feat(SI-2628): added geolocator_status.dart

---
+++ lib/features/common/domain/entity/geolocator/geolocator_status.dart
@@ -0,0 +1,11 @@
+/// Geolocator statuses
+enum GeolocatorStatus {
+  /// location service disable
+  locationServiceDisabled,
+
+  /// location permission denied
+  permissionDenied,
+
+  /// location permission blocked
+  permissionDeniedForever;
+}
