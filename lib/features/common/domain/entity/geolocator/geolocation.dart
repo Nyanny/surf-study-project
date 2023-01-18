04cd95809c483e2458af6e2204c3ed17c840c2c4
Nyanny <EvgenyNedosekin@gmail.com>
2023-01-18 16:03:38 +0300

    feat (SI-2628): added geolocation.dart

---
+++ lib/features/common/domain/entity/geolocator/geolocation.dart
@@ -0,0 +1,11 @@
+/// Class [Geolocation] is a data class holding coordinates
+class Geolocation {
+  /// longitude
+  final double longitude;
+
+  /// latitude
+  final double latitude;
+
+  /// constructor
+  Geolocation({required this.longitude, required this.latitude});
+}
