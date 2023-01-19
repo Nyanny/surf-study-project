da577f1708c7679baa5831eb096e2e902ce79a05
Nyanny <EvgenyNedosekin@gmail.com>
2023-01-20 00:07:13 +0300

    feat(SI-2628): added geolocation bloc to model and wm

--- lib/features/splash_screen/screens/splash_screen_model.dart
+++ lib/features/splash_screen/screens/splash_screen_model.dart
@@ -1,15 +1,47 @@
+import 'dart:async';
+
 import 'package:elementary/elementary.dart';
+import 'package:flutter/foundation.dart';
 import 'package:surf_study_project/features/common/service/app_settings_service.dart';
+import 'package:surf_study_project/features/geolocation/bloc/geolocation_bloc.dart';
 
 /// Class [SplashScreenModel] is model class for SplashScreen
 class SplashScreenModel extends ElementaryModel {
+  late final Stream<GeolocationState> _geolocationStateStream;
+
   /// [AppSettingsService]
   AppSettingsService appSettingsService;
 
+  /// [GeolocationBloc] instance
+  GeolocationBloc geolocationBloc;
+
+  /// gets current bloc state
+  GeolocationState get currentState => geolocationBloc.state;
+
+  /// Stream of [GeolocationState]
+  Stream<GeolocationState> get geolocationStateStream =>
+      _geolocationStateStream;
+
+  /// verifies locations service and app permission
+  VoidCallback get verifyPermissions => _verifyPermissions;
+
   /// constructor
-  SplashScreenModel({required this.appSettingsService});
+  SplashScreenModel({
+    required this.appSettingsService,
+    required this.geolocationBloc,
+  });
+
+  @override
+  void init() {
+    super.init();
+    _geolocationStateStream = geolocationBloc.stream;
+  }
 
   /// function that get onboarding status: is passed or not
   Future<bool> onboardingPassed() async =>
       appSettingsService.getOnboardingStatus();
+
+  void _verifyPermissions() {
+    geolocationBloc.add(VerifyThenRequestPermissionEvent());
+  }
 }
